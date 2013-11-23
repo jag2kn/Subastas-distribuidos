3c4ee4734ac228d14edfbb547171252f302adcd2
Jorge Gonzalez <jag2kn@gmail.com>
2013-11-23 13:31:26 -0500

    Se agrega versión inicial de un cliente para el aplicativo

---
+++ Cliente/Subasta/src/subasta/Subasta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subasta;
+
+/**
+ *
+ * @author AndrésFelipe
+ */
+public class Subasta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
