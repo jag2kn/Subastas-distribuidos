e6cfae8a7eae446cfb0320b5f15232dd66eb3a0b
Jorge Gonzalez <jag2kn@gmail.com>
2013-11-23 13:29:57 -0500

    Se agrega primera versión del servidor funcional
    Ver el README.md
    
    Proyecto basado en
    
    https://github.com/pk11/reactor-http-demo

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-Subastas-distribuidos
-=====================
+
+### Servidor de subastas
+
+Servidor realizado sobre Reactor para peticiones de diferentes clientes
+
+
+Ejemplo de peticiones
+
+Realizar login con el usuario Pepe
+
+ localhost:9955/login/Pepe
+
+Ver el estado actual de la subasta
+
+ localhost:9955/estado
+ 
+Realizar una oferta en la subasta del usuario Pepe por 500
+
+ localhost:9955/ofertar/500/Pepe
+
+
+### Para compilar:
+
+ mvn clean compile
+
+### Para correr:
+
+ mvn exec:java -Dexec.mainClass="com.github.pk11.rnio.scala.DemoHttpServer"
+
+
+
+Luego visitar: `http://localhost:9955`
---
+++ src/main/java/com/github/pk11/rnio/HttpServer.java
@@ -0,0 +1,386 @@
+package com.github.pk11.rnio;
+
+//import reactor.R;
+import reactor.core.spec.Reactors;
+import reactor.core.Environment;
+import reactor.core.Reactor;
+import reactor.event.Event;
+import reactor.function.Consumer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.SelectableChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.net.StandardSocketOptions;
+
+//import static reactor.function.Functions.$;
+import static reactor.event.selector.Selectors.$;
+
+
+public class HttpServer implements Runnable {
+
+	protected final Selector            selector = openSelector();
+	protected final ServerSocketChannel server   = openChannel();
+
+
+	public final Reactor reactor = Reactors.reactor()
+																	.env(new Environment())
+																	.dispatcher(Environment.RING_BUFFER)
+																	.get();
+
+
+	public HttpServer handler(final Handler rhandler) {
+		reactor.on($("channelhandler"), new Consumer<Event<SocketChannel>>() {
+			public void accept(Event<SocketChannel> ev) {
+				try {
+					SocketChannel channel = ev.getData();
+					if (channel.isOpen()) {
+						HTTPRequest request = new HTTPRequest(read(toBuffer(channel)));
+						rhandler.handle(request, new HTTPResponse(channel));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					sneakyThrow(e);
+				}
+
+			}
+		});
+
+		return this;
+	}
+
+	public HttpServer(int port) {
+		try {
+			server.socket().bind(new InetSocketAddress(port));
+			server.configureBlocking(false);
+			server.register(selector, SelectionKey.OP_ACCEPT);
+		} catch (IOException ex) {
+			sneakyThrow(ex);
+		}
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				int cnt = selector.select();
+				if (cnt > 0) {
+					Iterator<SelectionKey> i = selector.selectedKeys().iterator();
+                    while (i.hasNext()) {
+                        SelectionKey key = i.next();
+                        i.remove();
+                        if (key.isValid()) {
+                            try {
+                                if (key.isAcceptable()) {
+                                    SocketChannel channel = server.accept();
+                                    channel.configureBlocking(false);
+                                    channel.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
+                                    channel.setOption(StandardSocketOptions.TCP_NODELAY, true);
+                                    channel.register(selector, SelectionKey.OP_READ);
+                                } else if (key.isReadable()) {
+                                    SocketChannel channel = (SocketChannel) key.channel();
+                                    reactor.notify("channelhandler", Event.wrap(channel));
+                                }
+                            } catch (Exception ex) {
+                                SelectableChannel s  = key.channel();
+                                System.err.println("client: " + s);
+                                ex.printStackTrace();
+                                try {
+                                    s.close();
+                                } catch (IOException ie) {
+                                    ie.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ex) {
+                try {
+                    selector.close();
+                    server.close();
+                } catch (IOException innerEx) {
+                    innerEx.printStackTrace();
+                }
+                throw new RuntimeException(ex);
+            }
+        }
+
+    }
+
+    protected String read(ByteBuffer buffer) throws IOException {
+        StringBuilder l = new StringBuilder();
+        StringBuilder lines = new StringBuilder();
+        while (buffer.hasRemaining()) {
+            char c = (char) buffer.get();
+            l.append(c);
+            if (c == '\n') {
+                lines.append(l);
+                l.setLength(0);
+            }
+        }
+        return lines.toString();
+    }
+
+    protected ByteBuffer toBuffer(SocketChannel channel) throws IOException {
+        final ByteBuffer buffer = ByteBuffer.allocate(2048);
+        buffer.limit(buffer.capacity());
+        int read = channel.read(buffer);
+        if (read == -1) throw new IOException("stream has ended");
+        buffer.flip();
+        buffer.position(0);
+        return buffer;
+    }
+    // ~~~ nested classes/interfaces ~~~
+
+
+    public static class HTTPRequest {
+
+        public final String method;
+        public final String location;
+        public final String version;
+        public final Map<String, String> headers = new HashMap<>();
+
+        public HTTPRequest(String raw) {
+            StringTokenizer tokenizer = new StringTokenizer(raw);
+            method = tokenizer.nextToken().toUpperCase();
+            location = tokenizer.nextToken();
+            version = tokenizer.nextToken();
+            String[] lines = raw.split("\r\n");
+            for (int i = 1; i < lines.length; i++) {
+                String[] keyVal = lines[i].split(":", 2);
+                headers.put(keyVal[0], keyVal[1]);
+            }
+        }
+
+    }
+
+    public static class HTTPResponse {
+
+        private final SocketChannel channel;
+        private final Charset charset = Charset.forName("UTF-8");
+        private final CharsetEncoder encoder = charset.newEncoder();
+        byte[] raw;
+        int responseCode = 200;
+        String responseReason = "OK";
+        public final String version = "HTTP/1.1";
+        public final Map<String, String> headers = new LinkedHashMap<>();
+
+
+
+        public HTTPResponse(SocketChannel channel) {
+            this.channel = channel;
+        }
+
+        public void end() {
+            headers.put("Date", new Date().toString());
+            headers.put("Server", "Reactor NIO Server");
+            headers.put("Connection", "close");
+            headers.put("Content-Length", Integer.toString(raw.length));
+            try {
+                //write top header
+                writeLine(channel, version + " " + responseCode + " " + responseReason);
+                //write response headers
+                writeHeaders(channel, this);
+                //write content
+                writeLine(channel, "");
+                channel.write(ByteBuffer.wrap(raw));
+                //close channel
+                channel.close();
+            } catch (IOException ex) {
+                sneakyThrow(ex);
+            }
+        }
+
+        public void chunked() {
+            headers.put("Date", new Date().toString());
+            headers.put("Server", "Reactor NIO Server");
+            headers.put("Transfer-Encoding", "chunked");
+            try {
+                //write top header
+                writeLine(channel, version + " " + responseCode + " " + responseReason);
+                //write headers
+                writeHeaders(channel, this);
+            }    catch (IOException ex) {
+                sneakyThrow(ex);
+            }
+
+        }
+
+        public void flushChunks() {
+            try {
+                writeLine(channel, "Content-Length" + ": " +  Integer.toString(raw.length));
+                channel.close();
+            } catch (IOException ex) {
+                sneakyThrow(ex);
+            }
+        }
+
+        public HTTPResponse code(int responseCode) {
+            this.responseCode = responseCode;
+            return this;
+        }
+
+        public HTTPResponse reason(String responseReason) {
+            this.responseReason = responseReason;
+            return this;
+        }
+
+        public HTTPResponse content(String content) {
+            this.raw = content.getBytes();
+            return this;
+        }
+
+        public HTTPResponse append(byte[] content) {
+            byte[] combined = new byte[this.raw.length + content.length];
+            System.arraycopy(this.raw, 0, combined, 0, this.raw.length);
+            System.arraycopy(content, 0, content, this.raw.length, content.length);
+            this.raw = combined;
+            return this;
+        }
+
+        public HTTPResponse writeChunk(byte[] content) {
+            try {
+                channel.write(ByteBuffer.wrap(content));
+            } catch (IOException ex) {
+                sneakyThrow(ex);
+            }
+            return append(content);
+        }
+
+        public HTTPResponse header(String key, String value) {
+            headers.put(key, value);
+            return this;
+        }
+
+        private void writeLine(SocketChannel channel, String line) throws IOException {
+            channel.write(encoder.encode(CharBuffer.wrap(line + "\r\n")));
+        }
+
+        private void writeHeaders(SocketChannel channel, HTTPResponse response) {
+            try {
+                for (Map.Entry<String, String> header : response.headers.entrySet()) {
+                    writeLine(channel, header.getKey() + ": " + header.getValue());
+                }
+            } catch (IOException ex) {
+                sneakyThrow(ex);
+            }
+        }
+    }
+    public static interface Handler {
+        public void handle(HTTPRequest request, HTTPResponse response);
+    }
+    // ~~~  utils  ~~~
+
+    //http://www.mail-archive.com/javaposse@googlegroups.com/msg05984.html
+    private static RuntimeException sneakyThrow(Throwable t) {
+        if ( t == null ) throw new NullPointerException("t");
+        HttpServer.<RuntimeException>sneakyThrow0(t);
+        return null;
+    }
+    @SuppressWarnings("unchecked")
+    private static <T extends Throwable> void sneakyThrow0(Throwable t)
+    throws T {
+        throw (T)t;
+    }
+    private Selector openSelector() {
+        Selector s = null;
+        try {
+            s = Selector.open();
+        } catch (IOException ex) {
+            sneakyThrow(ex);
+        }
+        return s;
+    }
+    private ServerSocketChannel openChannel() {
+        ServerSocketChannel s = null;
+        try {
+            s = ServerSocketChannel.open();
+        } catch (IOException ex) {
+            sneakyThrow(ex);
+        }
+        return s;
+    }
+
+    // ~~~  app code ~~~
+
+    public static class DemoHandler implements Handler {
+    
+    	public String nombres="";
+    	public String ofertanteMayor="";
+    	public int valor=0;
+        public void handle(HTTPRequest request, HTTPResponse response) {
+            //note:
+            //this is single-thread so any serious work would need to be
+            //executed on a separate thread
+            
+            //nombres
+            System.out.println(request.location);
+            
+            String []parametros = request.location.split("/");
+            
+            
+            
+            String respuesta="Sin Respuesta";
+            
+            if (parametros.length>=2){
+	            switch(parametros[1]){
+	            	case "login":
+	            		respuesta = "Ingresando al sistema";
+	            		if (parametros.length>=3){
+		            		nombres += parametros[2]+", ";
+	            		}else{
+	            			respuesta = "Es necesario un nombre para el login";
+	            		}
+	            		break;
+	            	case "estado":
+	            		respuesta = "La mayor oferta es: "+valor+" hecha por "+ofertanteMayor;
+	            		break;
+	            	case "ofertar":
+	            		if (parametros.length>=4){
+	            			int ofertaNueva=Integer.parseInt(parametros[2]);
+	            			if (valor<ofertaNueva){
+	            				valor = ofertaNueva;
+	            				ofertanteMayor = parametros[3];
+	            				respuesta = "Nueva oferta realizada "+valor+" por "+ofertanteMayor;
+	            			}else{
+	            				respuesta = "Oferta menor a la actual<br>";
+	            				respuesta = "La mayor oferta es: "+valor+" hecha por "+ofertanteMayor;
+	            			}
+	            		}else{
+	            			respuesta = "Parametros incorrectos en la oferta";
+	            		}
+	            		break;
+	            	default:
+	            		respuesta = "Respuesta sin procesar ["+parametros[0]+","+parametros[1]+"]";
+	            		break;
+	            }
+            	
+            }
+
+            response.content("<h2>"+request.location+"</h2>Respuesta: "+respuesta+"<br>Nombres: "+nombres).end();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 9955;
+        HttpServer httpServer = new HttpServer(port).handler(new DemoHandler());
+        System.out.println("startup up a server at port:" + port + " waiting for requests...");
+        httpServer.run();
+    }
+
+}
